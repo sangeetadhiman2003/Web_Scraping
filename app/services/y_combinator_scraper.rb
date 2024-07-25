831741cbf98b419195ca56825bb8e0c7abc2eeed
sangeeta dhiman <=sangeetadhiman2003@gmail.com>
2024-07-25 19:04:39 +0530

    headless option added

--- app/services/y_combinator_scraper.rb
+++ app/services/y_combinator_scraper.rb
@@ -7,12 +7,11 @@ class YCombinatorScraper
     @url = url
     @new_url_base = new_url_base
     @limit = limit
-
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-
     @driver = Selenium::WebDriver.for :chrome, options: options
   end
 
